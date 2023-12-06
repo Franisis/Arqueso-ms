20b94791838ef23b86978608e45918a42b44c692
Franisis <f.guzmana@uniandes.edu.co>
2023-12-05 22:56:29 -0500

    fastAPI conection attempt

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
+from databases import Database
+from sqlalchemy.sql import select
 
+import asyncio
 
 app = FastAPI() #instancia de la aplicación
+
+USERNAME = 'jumafe2'
+PASSWORD = 'isis2503'
+NAME_MEW = '10.128.0.4'
+NAME_DB = 'citas_db'
+DATABASE_URL = 'postgresql://'+USERNAME+':'+PASSWORD+'@'+NAME_DB+'5432'+'/'+NAME_DB
+
+metadata=MetaData()
+database = Database(DATABASE_URL)
+
+
+async def startup_db():
+    try:
+        await database.connect()
+        return database
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        raise
+
+def shutdown():
+    return database.disconnect()
+
+app.add_event_handler("startup", startup_db)
+app.add_event_handler("shutdown", shutdown)
+
 
 @app.get('/')
 def message():
@@ -14,4 +43,11 @@ def getCitas():
 
 @app.get('/citas/{}')
 def getcitaby(algo):
+    pass
+
+#citas/?cualquierCosa como cedula o fecha o doctor que atiende
+
+@app.get('/citas/{}')
+def getCitasbysomeshit(someshit):
+    #return list(filter(lambda item['someshit'] == stock, products))
     pass
