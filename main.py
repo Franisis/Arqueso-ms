03f41030817ecb0309069f27e49a701830c51550
Franisis <f.guzmana@uniandes.edu.co>
2023-12-06 15:24:37 -0500

    POST citas

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def create_post(db: Session, post: Cita):
     db.refresh(db_post)
     
 
-@app.post('/citas/')
+@app.post('/citasCreate/')
 async def createCita(post: Cita):
     create_post(db=session, post=post)
     return "Clean"
