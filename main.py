0d2765a7e91f8e7761446dda5ec3df57b447eb80
Franisis <f.guzmana@uniandes.edu.co>
2023-12-06 02:04:43 -0500

    get citas(all)

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class Cita(Base):
     id = Column(Integer, primary_key=True, index=True)
     paciente = Column(String, index=True)
     medico = Column(String, index=True)
-    fecha = Column(date)
+    fecha = Column(types.DATETIME)
     nota = Column(String)
 
 citas_table = Table(
